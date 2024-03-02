8c48fde16decacfbd8e1bdb996f0c65c415b8566
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2024-03-02 14:45:02 -0500

    expose port

--- integration/run.sh
+++ integration/run.sh
@@ -2,12 +2,13 @@
 
 python3 -m pip install -e . --no-cache-dir
 
-while true; do
+attempts=0
+while [ $attempts -lt 30 ]; do
     if curl -s http://lila:9663 >/dev/null; then
         break
     fi
-    echo "⌛ Waiting for lila to start..."
     sleep 1
+    attempts=$((attempts + 1))
 done
 
 python3 integration/sample.py
