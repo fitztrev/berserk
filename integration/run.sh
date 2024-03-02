44f1390d2c24f80d481830f9a3af25937d6dacbe
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2024-03-02 14:56:33 -0500

    install curl

--- integration/run.sh
+++ integration/run.sh
@@ -7,6 +7,7 @@ while [ $attempts -lt 30 ]; do
     if curl -s http://lila:9663 >/dev/null; then
         break
     fi
+    echo "⌛ Waiting for lila to start..."
     sleep 1
     attempts=$((attempts + 1))
 done
