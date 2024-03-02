343ff8a60e1e8a63c474b6bf4ef20c649ec86bd3
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2024-03-02 14:59:26 -0500

    fail test

--- integration/sample.py
+++ integration/sample.py
@@ -4,10 +4,9 @@ session = berserk.TokenSession('lip_bobby')
 client = berserk.Client(session, base_url="http://lila:9663")
 
 me = client.account.get()
-assert me['id'] == 'bobby'
+assert me['id'] == 'bobby123'
 
-email = client.account.get_email()
-assert email == 'bobby@localhost'
+assert client.account.get_email() == 'bobby@localhost'
 
 assert client.account.get_kid_mode() == False
 client.account.set_kid_mode(True)
