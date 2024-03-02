fc7a16d2af4aea53464f96532954c44ac1be4020
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2024-03-02 15:01:06 -0500

    matrix

--- integration/sample.py
+++ integration/sample.py
@@ -4,7 +4,7 @@ session = berserk.TokenSession('lip_bobby')
 client = berserk.Client(session, base_url="http://lila:9663")
 
 me = client.account.get()
-assert me['id'] == 'bobby123'
+assert me['id'] == 'bobby'
 
 assert client.account.get_email() == 'bobby@localhost'
 
